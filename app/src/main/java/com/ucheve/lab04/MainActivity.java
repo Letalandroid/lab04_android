407f8ad565c38247f10849a80a6116da48a8af17
Ucv <ucv@ucv.edu.pe>
2024-09-26 10:43:55 -0500

    Toast finish

--- app/src/main/java/com/ucheve/lab04/MainActivity.java
+++ app/src/main/java/com/ucheve/lab04/MainActivity.java
@@ -1,24 +1,50 @@
 package com.ucheve.lab04;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    ArrayList<String> listaop;
+    ArrayAdapter<String> adaptador;
+    ListView lstOpciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        lstOpciones = findViewById(R.id.lstOpciones);
+        lstOpciones.setOnItemClickListener(this);
+
+        listaop = cargar();
+        adaptador = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaop);
+        lstOpciones.setAdapter(adaptador);
+    }
+
+    public ArrayList<String> cargar() {
+        listaop = null;
+        listaop = new ArrayList<>();
+        String obj = new String("Registrar");
+        listaop.add(obj);
+
+        String obj2 = new String("Listar");
+        listaop.add(obj2);
+
+        return listaop;
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String mensaje = lstOpciones.getItemAtPosition(position).toString();
+        Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_LONG).show();
     }
 }
